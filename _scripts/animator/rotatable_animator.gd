b70bb134e490ee08acabe6fe10bb6b32bb913e1c
IvanKorchmit <criperadmiral92@gmail.com>
2023-06-08 23:22:36 +0300

    Signals

--- _scripts/animator/rotatable_animator.gd
+++ _scripts/animator/rotatable_animator.gd
@@ -2,17 +2,29 @@ extends Sprite2D
 
 class_name RotatableAnimator
 
-@export var animation : Resource
-var angle : float = 0
+@export var animation : Dictionary
 @export var fps : float = 30
-var time : float = 0
 @export var currentFrame : int = 0
+
+signal on_frame_changed()
+signal on_animation_end()
+signal on_animation_start()
+
+var angle : float = 0
+var _time : float = 0
+
+
+
 func _process(delta):
-	time += delta
-	if time >= 1 / fps:
+	_time += delta
+	if _time >= 1 / fps:
+		on_frame_changed.emit()
+		
 		currentFrame += 1
 		currentFrame %= $CurrentAnimation.frameCount
-		time -= time
+		if currentFrame == 0:
+			on_animation_end.emit()
+		_time -= _time
 	texture = $CurrentAnimation.get_rotated_frame(angle, currentFrame)
 	
 func _ready():
