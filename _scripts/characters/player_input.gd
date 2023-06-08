8dbb746954cd949e2f3ac2653cdbb9c93dd86996
IvanKorchmit <criperadmiral92@gmail.com>
2023-06-09 02:39:29 +0300

    Aim

--- _scripts/characters/player_input.gd
+++ _scripts/characters/player_input.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 @export var movement : Movement
 @export var animator : RotatableAnimator
+
 func _process(delta):
 	var dir : Vector2 = Input.get_vector("left", "right", "up", "down")
+	
 	animator.angle = vec_to_deg(get_local_mouse_position() - position)
+	animator.set_animation("walk" if dir != Vector2.ZERO else "idle")
+	
 	movement.direction = dir
 	
 static func vec_to_deg(direction : Vector2) -> float:
