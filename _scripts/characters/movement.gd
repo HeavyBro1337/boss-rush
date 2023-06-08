e9ceeb113647fda9ad99b5bc0e488ecad654a400
IvanKorchmit <criperadmiral92@gmail.com>
2023-06-08 17:29:05 +0300

    New content

---
+++ _scripts/characters/movement.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+class_name Movement
+
+@export var speed : float = 300
+@export var acceleration : float = 0
+@export var deceleration : float = 0
+var direction : Vector2
+var currentVelocity : Vector2
+
+func _physics_process(delta):
+	var vel : Vector2 = currentVelocity.move_toward(direction, 
+		(acceleration if direction != Vector2.ZERO else deceleration) * delta) 
+	currentVelocity = vel
+	position += currentVelocity * speed * delta
+	
+	move_and_slide()
