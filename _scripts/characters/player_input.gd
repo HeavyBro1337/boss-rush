a3bb53e70c0d25d3e851ec710211a7109d0b8e91
IvanKorchmit <criperadmiral92@gmail.com>
2023-06-09 01:34:38 +0300

    Fix dependencies. Add aim

--- _scripts/characters/player_input.gd
+++ _scripts/characters/player_input.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 @export var movement : Movement
-
+@export var animator : RotatableAnimator
 func _process(delta):
 	var dir : Vector2 = Input.get_vector("left", "right", "up", "down")
+	animator.angle = vec_to_deg(get_local_mouse_position() - position)
 	movement.direction = dir
+	
+static func vec_to_deg(direction : Vector2) -> float:
+	return rad_to_deg(atan2(direction.y, direction.x))
